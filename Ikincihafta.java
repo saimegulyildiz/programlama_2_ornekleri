32247f3659f869822e9020103acbed17a08a2c76
saimegulyildiz <yildizsaimegul@gmail.com>
2026-05-02 22:58:58 +0300

    Add files via upload

---
+++ Ikincihafta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ikincihafta;
+
+/**
+ *
+ * @author acer
+ */
+public class Ikincihafta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
